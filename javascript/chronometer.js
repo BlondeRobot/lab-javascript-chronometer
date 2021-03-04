bf39fbda62ca0d11735d06116e56b79bbe498fca
Alicia Kmak <alicia.kmak@gmail.com>
2021-03-04 13:31:17 +0100

    done and working ok, jasmine tests all ok

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,26 +1,48 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
-  }
+    this.currentTime = 0;
+    this.intervalId = 0;
+  };
+
   startClick(callback) {
-    // ... your code goes here
-  }
+    const intervalId = setInterval(() => {this.currentTime ++}, 1000);
+  };
+
   getMinutes() {
-    // ... your code goes here
+    if (this.currentTime === 0) {return 0;}
+    else { let minutes = Math.floor(this.currentTime / 60);
+    return minutes;
+    }
   }
   getSeconds() {
-    // ... your code goes here
+    let seconds = this.currentTime
+    if (this.currentTime === 0) {return 0;}
+    else if (this.currentTime % 60 !== 0); {
+    return seconds % 60;}
+    
   }
-  twoDigitsNumber() {
-    // ... your code goes here
+  
+  twoDigitsNumber(time) {
+    if (time < 10) {
+      return `0${time.toString()}`;
+    } else {
+      return time.toString();
+    }
   }
+
   stopClick() {
-    // ... your code goes here
-  }
+    return clearInterval(this.intervalId)
+  } // return {clearInterval(this.intervalId)} wasn't working until I removed the curly brackets. Why?
+
+
   resetClick() {
-    // ... your code goes here
-  }
+     return this.currentTime = 0
+   }
+
   splitClick() {
-    // ... your code goes here
+    let min = this.twoDigitsNumber(this.getMinutes());
+    let sec = this.twoDigitsNumber(this.getSeconds());
+    
+    return `${min}:${sec}`
   }
 }
